75dec8fd56c8eef54c1f76f641f4d1a9b7187049
Newton Ni <nwtnni@gmail.com>
2024-09-09 16:35:02 -0500

    Add duct library and run perf

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
-use std::process::Command;
 
 use clap::Parser;
+use duct::cmd;
 
 use mimalloc_bench::Allocator;
 use mimalloc_bench::Benchmark;
@@ -9,6 +9,17 @@ use mimalloc_bench::Benchmark;
 #[derive(Parser)]
 enum Cli {
     Record {
+        #[arg(short, long)]
+        allocator: Allocator,
+
+        #[arg(short, long)]
+        benchmark: Benchmark,
+
+        #[arg(short, long)]
+        release: bool,
+    },
+
+    Perf {
         #[arg(short, long)]
         allocator: Allocator,
 
@@ -32,17 +43,57 @@ fn main() {
                 .canonicalize()
                 .unwrap();
 
-            let status = Command::new("rr")
-                .arg("record")
-                .arg(format!("--env=LD_PRELOAD={}", path.display()))
-                .arg(benchmark.path())
-                .args(benchmark.args())
-                .spawn()
-                .unwrap()
-                .wait()
+            cmd![
+                "rr",
+                "record",
+                format!("--env=LD_PRELOAD={}", path.display()),
+                benchmark.path(),
+            ]
+            .before_spawn(move |command| {
+                command.args(benchmark.args());
+                Ok(())
+            })
+            .run()
+            .unwrap();
+        }
+        Cli::Perf {
+            allocator,
+            benchmark,
+            release,
+        } => {
+            let path = Path::new("extern")
+                .join(allocator.path(release))
+                .canonicalize()
                 .unwrap();
 
-            assert!(status.success());
+            cmd![
+                "perf",
+                "record",
+                "--call-graph",
+                "dwarf",
+                "-o",
+                "perf.data",
+                "env",
+                format!("LD_PRELOAD={}", path.display()),
+                benchmark.path(),
+            ]
+            .before_spawn(move |command| {
+                command.args(benchmark.args());
+                Ok(())
+            })
+            .run()
+            .unwrap();
+
+            cmd!["perf", "script", "--input", "perf.data"]
+                .pipe(cmd!(std::env::home_dir()
+                    .unwrap()
+                    .join(".cargo/bin/inferno-collapse-perf")))
+                .pipe(cmd!(std::env::home_dir()
+                    .unwrap()
+                    .join(".cargo/bin/inferno-flamegraph")))
+                .stdout_path("out.svg")
+                .run()
+                .unwrap();
         }
     }
 }
